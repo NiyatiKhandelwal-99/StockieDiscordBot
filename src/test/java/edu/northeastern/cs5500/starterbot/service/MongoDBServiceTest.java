365370e5444fe317c9d327d84e9cd95c0f8b1d10
akuten1298 <51192400+akuten1298@users.noreply.github.com>
2023-03-22 03:33:24 +0000

    Added db crud test commands

--- src/test/java/edu/northeastern/cs5500/starterbot/service/MongoDBServiceTest.java
+++ src/test/java/edu/northeastern/cs5500/starterbot/service/MongoDBServiceTest.java
@@ -1,12 +1,26 @@
 package edu.northeastern.cs5500.starterbot.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.result.UpdateResult;
 
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.Nested;
+
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Updates.set;
+
+import org.bson.Document;
+
+@TestMethodOrder(OrderAnnotation.class)
 class MongoDBServiceTest {
 
     private MongoDBService getMongoDBService() {
@@ -14,11 +28,13 @@ class MongoDBServiceTest {
     }
 
     @Test
+    @Order(1)
     void testGetMongoDBService() {
         assertNotNull(getMongoDBService());
     }
 
     @Test 
+    @Order(2)
     void testMongoDBCollections() {
         MongoDBService mongoDBService = getMongoDBService();
         MongoClient mongoClient = mongoDBService.getMongoClient();
@@ -26,6 +42,56 @@ class MongoDBServiceTest {
 
         assertNotNull(mongoClient);
         assertNotNull(mongoDatabase);
+    }
+
+    @Test 
+    @Order(3)
+    void testMongoDBCollectionInsertion() {
+        MongoDBService mongoDBService = getMongoDBService();
+        MongoDatabase mongoDatabase = mongoDBService.getMongoDatabase();
+
+        MongoCollection<Document> collection = mongoDatabase.getCollection("test");
+        Document document = new Document("field_name", "field_value")
+                .append("_id", "primary_key");
+
+        collection.insertOne(document);
+    }
+
+    @Test 
+    @Order(4)
+    void testMongoDBCollectionUpdate() {
+        MongoDBService mongoDBService = getMongoDBService();
+        MongoDatabase mongoDatabase = mongoDBService.getMongoDatabase();
+
+        MongoCollection<Document> collection = mongoDatabase.getCollection("test");
+
+        UpdateResult updateResult = collection.updateOne(eq("_id", "primary_key"), 
+        set("field_name", "updated_field_value"));
+
+        assertTrue(updateResult.getModifiedCount() > 0);
+        assertEquals(1, collection.countDocuments());
+        long count = collection.countDocuments(eq("_id", "primary_key"));
+        assertEquals(1, count);
+    }
+
+    @Test 
+    @Order(5)
+    void testMongoDBCollectionDelete() {
+        MongoDBService mongoDBService = getMongoDBService();
+        MongoClient mongoClient = mongoDBService.getMongoClient();
+        MongoDatabase mongoDatabase = mongoDBService.getMongoDatabase();
+
+        MongoCollection<Document> collection = mongoDatabase.getCollection("test");
+
+        System.out.println("Total Documents: " + collection.countDocuments());
+
+        long count = collection.countDocuments(eq("_id", "primary_key"));
+        assertEquals(1, count);
+
+        collection.findOneAndDelete(eq("_id", "primary_key"));
+
+        count = collection.countDocuments(eq("_id", "primary_key"));
+        assertEquals(0, count);
 
         mongoClient.close();
     }
