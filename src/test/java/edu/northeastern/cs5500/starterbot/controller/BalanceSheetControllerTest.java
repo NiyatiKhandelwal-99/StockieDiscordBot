881fa4ba595c3c353d36602a8fe8ed7a8cdbcf59
akuten1298 <51192400+akuten1298@users.noreply.github.com>
2023-04-18 14:33:43 +0000

    updated testing

--- src/test/java/edu/northeastern/cs5500/starterbot/controller/BalanceSheetControllerTest.java
+++ src/test/java/edu/northeastern/cs5500/starterbot/controller/BalanceSheetControllerTest.java
@@ -3,9 +3,7 @@ package edu.northeastern.cs5500.starterbot.controller;
 import static com.google.common.truth.Truth.assertThat;
 
 import org.junit.Test;
-import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
-@EnabledIfEnvironmentVariable(named = "ALPHA_VANTAGE_API_KEY", matches = ".+")
 public class BalanceSheetControllerTest {
 
     private BalanceSheetController getBalanceSheetController() {
