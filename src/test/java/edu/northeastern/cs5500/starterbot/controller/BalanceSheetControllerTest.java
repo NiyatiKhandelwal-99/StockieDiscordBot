e056cb8632c80f062aefee6d9d84cab46c300615
akuten1298 <51192400+akuten1298@users.noreply.github.com>
2023-04-18 18:46:00 +0000

    updated coverage

--- src/test/java/edu/northeastern/cs5500/starterbot/controller/BalanceSheetControllerTest.java
+++ src/test/java/edu/northeastern/cs5500/starterbot/controller/BalanceSheetControllerTest.java
@@ -3,6 +3,9 @@ package edu.northeastern.cs5500.starterbot.controller;
 import static com.google.common.truth.Truth.assertThat;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import edu.northeastern.cs5500.starterbot.exception.rest.BadRequestException;
 
 public class BalanceSheetControllerTest {
 
@@ -21,6 +24,23 @@ public class BalanceSheetControllerTest {
 
         for (String tickerSymbol : validTickerSymbols) {
             assertThat(balanceSheetController.getBalanceSheet(tickerSymbol)).isNotNull();
+        }
+    }
+    @Test
+    public void testInvalidTickerSymbolForBalanceSheet() throws Exception {
+        BalanceSheetController balanceSheetController = getBalanceSheetController();
+        final String[] invalidTickerSymbols = {
+            null, ""
+        };
+
+        for (String tickerSymbol : invalidTickerSymbols) {
+            try {
+                System.out.println(tickerSymbol);
+                balanceSheetController.getBalanceSheet(tickerSymbol);
+                fail("Incorrect Balance Sheet command request failed");
+            } catch (BadRequestException e) {
+                assertThat(e.getMessage().length() > 0);
+            }
         }
     }
 }
