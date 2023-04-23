ceb31ec044132978cd1ddf5fdd035d9eacc184b5
NiyatiKhandelwal-99 <66090754+NiyatiKhandelwal-99@users.noreply.github.com>
2023-04-23 15:25:10 +0000

    Re-factoring

--- src/main/java/edu/northeastern/cs5500/starterbot/command/UpVoteCommand.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/command/UpVoteCommand.java
@@ -4,7 +4,6 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Updates;
-
 import edu.northeastern.cs5500.starterbot.annotate.Generated;
 import edu.northeastern.cs5500.starterbot.constants.LogMessages;
 import edu.northeastern.cs5500.starterbot.service.MongoDBService;
@@ -12,6 +11,7 @@ import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
@@ -21,6 +21,10 @@ import org.bson.Document;
 @Singleton
 @Slf4j
 public class UpVoteCommand implements SlashCommandHandler {
+
+    public static final String VOTES = "votes";
+    public static final String VOTERS = "voters";
+    public static final String TICKER = "ticker";
 
     @Inject MongoDBService mongoDBService;
 
@@ -39,7 +43,7 @@ public class UpVoteCommand implements SlashCommandHandler {
         return Commands.slash(getName(), "Ask the bot to upvote for a particular ticker")
                 .addOption(
                         OptionType.STRING,
-                        "ticker",
+                        TICKER,
                         "The bot will upvote the provided ticker",
                         true);
     }
@@ -49,9 +53,14 @@ public class UpVoteCommand implements SlashCommandHandler {
     public void onSlashCommandInteraction(@Nonnull SlashCommandInteractionEvent event) {
 
         log.info("event: /upvote");
-        var option = event.getOption("ticker");
-        
-        String userId = event.getMember().getId();
+        var option = event.getOption(TICKER);
+
+        String userId = null;
+
+        if (event.getMember() != null) {
+            Member m = event.getMember();
+            if (m != null) userId = m.getId();
+        }
 
         if (option == null) {
             log.error(LogMessages.EMPTY_TICKER, event.getName());
@@ -64,22 +73,17 @@ public class UpVoteCommand implements SlashCommandHandler {
 
         MongoDatabase mongoDatabase = mongoDBService.getMongoDatabase();
 
-        MongoCollection<Document> collection = mongoDatabase.getCollection("votes");
+        MongoCollection<Document> collection = mongoDatabase.getCollection(VOTES);
 
-        Document document = collection.find(Filters.eq("ticker", ticker)).first();
+        Document document = collection.find(Filters.eq(TICKER, ticker)).first();
         if (document == null) {
             event.reply(LogMessages.INVALID_TICKER).queue();
         } else {
             // Check if the user has already voted for this ticker
-            boolean userHasVoted = document.getList("voters", String.class).contains(userId);
+            boolean userHasVoted = hasUserVoted(document, userId);
 
             if (!userHasVoted) {
-                // The user has not voted for this ticker, so update the votes and add the user to the voters list
-                collection.updateOne(Filters.eq("ticker", ticker), Updates.combine(
-                    Updates.inc("votes", 1),
-                    Updates.addToSet("voters", userId)
-                ));
-
+                increaseCount(collection, ticker, userId);
                 event.reply("You have successfully upvoted for the ticker " + ticker + ".").queue();
 
             } else {
@@ -88,16 +92,13 @@ public class UpVoteCommand implements SlashCommandHandler {
         }
     }
 
-    public Document createDocumentWithTicker(String ticker) {
-        return new Document("ticker", ticker);
+    public Boolean hasUserVoted(Document document, String userId) {
+        return document.getList(VOTERS, String.class).contains(userId);
     }
 
-    @Generated
-    public int collectionCount(MongoCollection<Document> collection, Document doc) {
-        return (int) collection.countDocuments(doc);
-    }
-
-    public Document createUpDateDocument() {
-        return new Document("$inc", new Document("votes", 1));
+    public void increaseCount(MongoCollection<Document> collection, String ticker, String userId) {
+        collection.updateOne(
+                Filters.eq(TICKER, ticker),
+                Updates.combine(Updates.inc(VOTES, 1), Updates.addToSet(VOTERS, userId)));
     }
 }
